499686bcb21eb5e333f5a9ee67602a79e2b3c2ca
Jim Weber <jim.weber@crosschx.com>
2017-07-06 15:05:15 -0400

    improved tests

--- e2e-test.sh
+++ e2e-test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-appname="kmantmpl"
+appname="kmt"
 go build
+
+## Test only reading values from file
 result=$(./$appname -i testfiles/test.yaml)
 testfile=$(cat testfiles/test-result.yaml)
 
@@ -10,5 +12,18 @@ then
     echo "Files do not match"
     echo "$(diff <(echo "$result") <(echo "$testfile"))"
 else
-    echo "Test Good!"
+    echo "File Only Test Good!"
 fi
+
+## test reading values from file and from cli.
+## cli values should override file
+result=$(./$appname -i testfiles/test.yaml -p version=2.2.2 -p name=testcontainer2)
+testfile=$(cat testfiles/test-result2.yaml)
+if [ "$result" != "$testfile" ] 
+then
+    echo "Files do not match"
+    echo "$(diff <(echo "$result") <(echo "$testfile"))"
+else
+    echo "File and CLI params Test Good!"
+fi
+
